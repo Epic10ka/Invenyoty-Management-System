b75d4523375ae05c509e8033bd4e460ccdcb7817
Epic10ka <lucasfortesrocha2@gmail.com>
2026-08-31 11:39:06 -0300

    data functions, main menu  construction and menu functions

--- inventory/imports.py
+++ inventory/imports.py
@@ -1,4 +1,6 @@
 from inventory.models import Item
 from inventory.functions import item_creation
+from rich import print
+from rich.panel import Panel
 
-__all__ = ['Item', 'item_creation', ]
+__all__ = ['Item', 'item_creation', 'print', 'Panel']
